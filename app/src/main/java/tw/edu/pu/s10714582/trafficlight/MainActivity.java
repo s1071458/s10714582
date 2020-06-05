e4434d1362429fb8327fb0f62a3d8c59ec241dd3
s1071458 <s1071458@gm.pu.edu.tw>
2020-06-05 15:44:24 +0800

    2020上機考版本2(歐陽書慶)

--- app/src/main/java/tw/edu/pu/s10714582/trafficlight/MainActivity.java
+++ app/src/main/java/tw/edu/pu/s10714582/trafficlight/MainActivity.java
@@ -1,13 +1,18 @@
 package tw.edu.pu.s10714582.trafficlight;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText greenText;
+    EditText yellowText;
+    EditText redText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +30,35 @@ public class MainActivity extends AppCompatActivity {
         //設定螢幕為橫式
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
-
         setContentView(R.layout.activity_main);
+
+        greenText = findViewById(R.id.et1);
+        yellowText = findViewById(R.id.et2);
+        redText = findViewById(R.id.et3);
     }
 
-    public void StartGame(View v){
-        Intent it = new Intent();
-        it.setClass(this, GameActivity.class);
-        startActivity(it);
+    public void StartGame(View v) {
+
+        String g = greenText.getText().toString();
+        String y = yellowText.getText().toString();
+        String r = redText.getText().toString();
+
+        if (g.equals("") || y.equals("") || r.equals("")) {
+            Toast.makeText(this, "燈號的秒數不能為空白", Toast.LENGTH_LONG).show();
+        } else if (g.equals("0") || y.equals("0") || r.equals("0")) {
+            Toast.makeText(this, "燈號的秒數不能為0", Toast.LENGTH_LONG).show();
+        } else {
+            Intent it = new Intent();
+            it.setClass(this, GameActivity.class);
+            it.putExtra("green", Integer.parseInt(g));
+            it.putExtra("yellow", Integer.parseInt(y));
+            it.putExtra("red", Integer.parseInt(r));
+            startActivity(it);
+            finish();
+        }
+    }
+
+    public void EndApp(View v) {
         finish();
     }
-
-    public void EndApp(View v){
-        finish();
-    }
-
-
 }
